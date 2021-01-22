570f73a2a10899122c0f3112d38ecc9a37123c6a
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2021-01-22 12:54:16 -0500

    Adds stripe integration for purchases. Changes favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51ICSmfF7XVJP04nmvA8zSFvg9jZmzrZvA4Ktmgy10QDtXtTDHpSqBv7TRZZ987W18j4BruiYCGNyUpIbY1STNHoZ00pABWPdZ4";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="FlashCat Clothing"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
