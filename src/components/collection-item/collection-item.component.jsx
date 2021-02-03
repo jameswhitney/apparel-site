75bbafe380342af1174f660a9e6e62766be89c7c
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2021-02-03 13:21:52 -0500

    Adds dynamic routing onclick for Titles in collection-preview component.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 
-// import CustomButton from "../custom-button/custom-button.component";
 import { addItem } from "../../redux/cart/cart.actions";
 
-// import "./collection-item.styles.scss";
 import {
   CollectionItemContainer,
   AddButton,
