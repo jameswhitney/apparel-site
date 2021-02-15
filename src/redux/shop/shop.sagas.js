29194cef935a28ec0666fb7878ee28e361619824
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2021-02-15 16:20:02 -0500

    Refactors async calls for user auth from redux-thunk to use redux-saga patterns for async auth

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,35 @@
+import { takeLatest, call, put } from "redux-saga/effects";
+
+import {
+  firestore,
+  convertCollectionsSnapShotToMap,
+} from "../../firebase/firebase.utils";
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
+
+import ShopActionTypes from "./shop.types";
+
+export function* fetchCollectionsAsync() {
+  yield console.log("I am fired");
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapShot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapShotToMap,
+      snapShot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
