f5d20095a979acedd96fa3cc7fb8559fead7a1d4
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2021-02-24 11:48:03 -0500

    removing gitignore files

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51ICSmfF7XVJP04nmvA8zSFvg9jZmzrZvA4Ktmgy10QDtXtTDHpSqBv7TRZZ987W18j4BruiYCGNyUpIbY1STNHoZ00pABWPdZ4";
 
-  const onToken = () => {
-    alert("Payment Successful");
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Processed");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was a issue while processing payment. Please make sure you use the provided credit card"
+        );
+      });
   };
 
   return (
@@ -16,7 +33,7 @@ const StripeCheckoutButton = ({ price }) => {
       name="FlashCat Clothing"
       billingAddress
       shippingAddress
-      image="https://sendeyo.com/up/d/f3eb2117da"
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
